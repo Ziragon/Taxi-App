23fbc77a59c6dd3c0c8ac992a4e2744fdeaf32e6
Krivenyshev Evgeniy <crive.ru@yandex.ru>
2026-04-22 22:55:10 +0700

    fix(user-service): обновил все Request классы на тип record

--- backend/user-service/src/main/java/com/example/userservice/dto/request/CreatePassengerProfileRequest.java
+++ backend/user-service/src/main/java/com/example/userservice/dto/request/CreatePassengerProfileRequest.java
@@ -3,26 +3,20 @@ package com.example.userservice.dto.request;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Schema(description = "Создание профиля пассажира")
-public class CreatePassengerProfileRequest {
+public record CreatePassengerProfileRequest (
 
     @NotBlank(message = "Имя обязательно")
     @Size(max = 100, message = "Максимум 100 символов")
     @Schema(description = "Имя", example = "Иван")
-    private String firstName;
+    String firstName,
 
     @NotBlank(message = "Фамилия обязательна")
     @Size(max = 100, message = "Максимум 100 символов")
     @Schema(description = "Фамилия", example = "Иванов")
-    private String lastName;
+    String lastName,
 
     @Schema(description = "URL фото профиля", example = "https://cults3d.com/en/3d-model/art/six-seven-meme-character?srsltid=AfmBOooc9X4Jb3lmH25XJC5dcAyY9E44M9HvZPvOJVCAIs7SFjeEx0v8")
-    private String photoUrl;
-}
+    String photoUrl
+) {}
--- backend/user-service/src/main/java/com/example/userservice/dto/request/UpdateDriverProfileRequest.java
+++ backend/user-service/src/main/java/com/example/userservice/dto/request/UpdateDriverProfileRequest.java
@@ -2,28 +2,22 @@ package com.example.userservice.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Schema(description = "Обновление профиля водителя")
-public class UpdateDriverProfileRequest {
+public record UpdateDriverProfileRequest (
 
     @Size(max = 100, message = "Максимум 100 символов")
     @Schema(description = "Имя", example = "Сергей")
-    private String firstName;
+    String firstName,
 
     @Size(max = 100, message = "Максимум 100 символов")
     @Schema(description = "Фамилия", example = "Петров")
-    private String lastName;
+    String lastName,
 
     @Size(max = 50, message = "Максимум 50 символов")
     @Schema(description = "Номер водительского удостоверения", example = "7799887766")
-    private String licenseNumber;
+    String licenseNumber,
 
     @Schema(description = "URL фото профиля", example = "https://cults3d.com/en/3d-model/art/six-seven-meme-character?srsltid=AfmBOooc9X4Jb3lmH25XJC5dcAyY9E44M9HvZPvOJVCAIs7SFjeEx0v8")
-    private String photoUrl;
-}
+    String photoUrl
+) {}
