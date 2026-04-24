d75f46222aa99711aec45043cbb7cbf94d7b04ca
Krivenyshev Evgeniy <crive.ru@yandex.ru>
2026-04-24 16:52:15 +0700

    feat(gateway-service): настроен SecurityConfig на гейтвее

---
+++ backend/gateway-service/src/main/java/com/example/gatewayservice/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.example.gatewayservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .cors(ServerHttpSecurity.CorsSpec::disable)
+                .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
+                .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
+                .anonymous(ServerHttpSecurity.AnonymousSpec::disable)
+                .authorizeExchange(auth -> auth.anyExchange().permitAll());
+
+        return http.build();
+    }
+}
