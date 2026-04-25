e539cc2e20e51552e05af8d6593ff2c542253c7e
Krivenyshev Evgeniy <crive.ru@yandex.ru>
2026-04-25 16:12:44 +0700

    feat(trip-service): добавлены базовые enum для сущностей

---
+++ backend/trip-service/src/main/java/com/example/tripservice/entity/enums/AccountStatus.java
@@ -0,0 +1,7 @@
+package com.example.tripservice.entity.enums;
+
+public enum AccountStatus {
+    PASSENGER,
+    DRIVER,
+    SYSTEM
+}
---
+++ backend/trip-service/src/main/java/com/example/tripservice/entity/enums/TripStatus.java
@@ -0,0 +1,10 @@
+package com.example.tripservice.entity.enums;
+
+public enum TripStatus {
+    CREATED,
+    SEARCHING,
+    DRIVER_ASSIGNED,
+    IN_PROGRESS,
+    COMPLETED,
+    CANCELLED
+}
