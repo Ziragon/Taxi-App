615024e5dbd84ccb08703c21baf060427a82fe0a
Krivenyshev Evgeniy <crive.ru@yandex.ru>
2026-04-26 22:01:06 +0700

    feat(trip-service): добавлена базовая инициализация тарифов в flyway

---
+++ backend/trip-service/src/main/resources/db/migration/V2__tariffs_init.sql
@@ -0,0 +1,5 @@
+INSERT INTO tariffs (id, trip_class, price_per_km, base_fare, price_per_min, is_active)
+VALUES
+    (1, 'ECONOMY', 15.0, 50.0, 100.0, true),
+    (2, 'COMFORT', 25.0, 80.0, 150.0, true),
+    (3, 'BUSINESS', 45.0, 150.0, 300.0, true);
