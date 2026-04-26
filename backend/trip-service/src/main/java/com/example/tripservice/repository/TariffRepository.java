316f0e53611e16f0d25af46f6bb6f10e7013093b
Krivenyshev Evgeniy <crive.ru@yandex.ru>
2026-04-26 19:59:32 +0700

    feat(trip-service): прописаны базовые репозитории

---
+++ backend/trip-service/src/main/java/com/example/tripservice/repository/TariffRepository.java
@@ -0,0 +1,9 @@
+package com.example.tripservice.repository;
+
+import com.example.tripservice.entity.Tariff;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TariffRepository extends JpaRepository<Tariff, Long> {
+}
