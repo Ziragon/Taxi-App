ff6f8bdc14d85fb480bbef260090cbf22b1db4f0
Krivenyshev Evgeniy <crive.ru@yandex.ru>
2026-04-27 18:20:38 +0700

    feat(trip-service):     1. добавлен Feign-клиент запроса к WeatherAPI     2. добавлена утилита расчета коэффициента цены по погоде

---
+++ backend/trip-service/src/main/java/com/example/tripservice/dto/response/WeatherResponse.java
@@ -0,0 +1,24 @@
+package com.example.tripservice.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record WeatherResponse(
+
+        Location location,
+
+        Current current
+) {
+    public record Location(String name) {}
+
+    public record Current(
+            @JsonProperty("temp_c") double tempC,
+
+            Condition condition
+    ) {}
+
+    public record Condition(
+            String text,
+
+            Integer code
+    ) {}
+}
