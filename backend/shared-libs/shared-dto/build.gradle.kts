002d2da057b599ae7c1ea57a674c48f2a8c1a4cc
Krivenyshev Evgeniy <crive.ru@yandex.ru>
2026-04-28 20:16:23 +0700

    fix(shared-dto):     1. добавлен api implementation в shared-dto     2. вынесен NearbyDriversRequest в shared

--- backend/shared-libs/shared-dto/build.gradle.kts
+++ backend/shared-libs/shared-dto/build.gradle.kts
@@ -6,4 +6,5 @@ val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
 
 dependencies {
     api(libs.findLibrary("jackson-databind").get())
+    api(libs.findLibrary("springdoc-webmvc-ui").get())
 }
