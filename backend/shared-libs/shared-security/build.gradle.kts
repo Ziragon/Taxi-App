a202b03d50ef27c9be35cfebd81149ddbd22f306
Krivenyshev Evgeniy <crive.ru@yandex.ru>
2026-04-29 17:47:43 +0700

    feat(auth-filter): настроен interceptor Feign-клиента для internal-запросов

--- backend/shared-libs/shared-security/build.gradle.kts
+++ backend/shared-libs/shared-security/build.gradle.kts
@@ -5,6 +5,14 @@ plugins {
 
 val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
 
+dependencyManagement {
+    imports {
+        val springCloudVersion = libs.findVersion("spring-cloud").get().requiredVersion
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:$springCloudVersion")
+    }
+}
+
 dependencies {
-    api(libs.findLibrary("boot-starter-webflux").get())
+    api(libs.findLibrary("boot-starter-webflux").get()) // есть во всех микросервисах
+    compileOnly(libs.findLibrary("boot-starter-openfeign").get()) // только для использующих Feign
 }
